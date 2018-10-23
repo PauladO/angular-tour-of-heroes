fe05252e240fd133a3829b8f6b065ec5d1f02612
Paula <pauladenouden@gmail.com>
2018-10-23 14:27:34 +0200

    Hero list, edit selected

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain Obvious' },
+  { id: 12, name: 'Killa Shoes' },
+  { id: 13, name: 'Fly McFlyface' },
+  { id: 14, name: 'Celerytas' },
+  { id: 15, name: 'Fridge Magneta' },
+  { id: 16, name: 'RubberDuckyMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr Smartypants' },
+  { id: 19, name: 'Sparky' },
+  { id: 20, name: 'Breeze' }
+]
